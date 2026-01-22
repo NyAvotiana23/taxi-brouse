b19c8466b76fa537a22db4f0b77b1ac22e1fb7dc
RAJAONSON Ny Andry Miaro <miaromiaro207@gmail.com>
2026-01-22 13:17:15 +0300

    test

--- src/main/java/com/mdgtaxi/service/TrajetService.java
+++ src/main/java/com/mdgtaxi/service/TrajetService.java
@@ -24,6 +24,7 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 public class TrajetService {
+    
 
     private final EntityManagerFactory emf = HibernateUtil.getEntityManagerFactory();
 
